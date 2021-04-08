055a6e0ab3b44fe993f3eef38333d344d8f89a9c
Payne Miller <paynemiller7792@gmail.com>
2021-04-08 17:48:43 -0400

    Fix typo; extract input/output from CharacterCounter

--- src/com/paynemiller/Runner.java
+++ src/com/paynemiller/Runner.java
@@ -1,15 +1,24 @@
 package com.paynemiller;
-import com.paynemiller.ch2.CharcterCounter;
+
+import com.paynemiller.ch2.CharacterCounter;
+import com.paynemiller.ch2.EmptyWordException;
+
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        CharcterCounter counter = new CharcterCounter();
+        CharacterCounter counter = new CharacterCounter();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please input a string.");
+        String input = keyboard.nextLine();
 
-        System.out.println(counter.countCharacters("ThisHasTen"));
-        System.out.println(counter.countCharacters("Test 2"));
-        System.out.println(counter.countCharacters(""));
-
-
+        try {
+            int numberOfCharacters = counter.countCharacters(input);
+            System.out.println("The word that was inputted was " + input + " and the number of characters it has is "
+                    + numberOfCharacters);
+        } catch (EmptyWordException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
 
