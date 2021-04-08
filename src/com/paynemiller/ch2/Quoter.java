563b3096656e1eb58e5f016a0f8e20ebec14a3d6
Payne Miller <paynemiller7792@gmail.com>
2021-04-08 14:14:42 -0400

    Add char counter and quotation exercise skeletons

---
+++ src/com/paynemiller/ch2/Quoter.java
@@ -0,0 +1,26 @@
+package com.paynemiller.ch2;
+
+/**
+ * Quotation marks are often used to denote the start and end of a string. But sometimes we need to print out the
+ * quotation marks themselves by using escape characters. Create a program that prompts for a quote and an author.
+ * Display the quotation and author as shown in the example output.
+ *
+ * Example Output
+ * What is the quote? These aren't the droids you're looking for. Who said it? Obi-Wan Kenobi
+ * Obi-Wan Kenobi says, "These aren't the droids
+ * you're looking for."
+ *
+ * Constraints
+ * • Use a single output statement to produce this output, using appropriate string-escaping techniques for quotes.
+ * • If your language supports string interpolation or string substitution, don’t use it for this exercise.
+ *   Use string concatenation instead.
+ *
+ * Challenge
+ * • I  Chapter7,Data Structures,on page 63,you’ll practice working with lists of data. Modify this program so that
+ *   instead of prompting for quotes from the user, you cre- ate a structure that holds quotes and their associated
+ *   attributions and then display all of the quotes using the format in the example.
+ *   An array of maps would be a good choice.
+ */
+public class Quoter {
+  
+}
