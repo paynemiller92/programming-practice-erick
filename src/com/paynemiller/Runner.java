1b1937b3392fce68423287c2cb61c113a9452250
Payne Miller <paynemiller7792@gmail.com>
2021-04-14 17:29:04 -0400

    Implement Exercise 3 challenge

--- src/com/paynemiller/Runner.java
+++ src/com/paynemiller/Runner.java
@@ -1,23 +1,12 @@
 package com.paynemiller;
 
-import com.paynemiller.ch2.CharcterCounter;
 import com.paynemiller.ch2.EscapeCharacters;
 
 public class Runner {
-    public static void main(String[] args) {
-        /*
-        CharcterCounter counter = new CharcterCounter();
-        System.out.println(counter.countCharacters("ThisHasTen"));
-        System.out.println(counter.countCharacters("Test 2"));
-        System.out.println(counter.countCharacters(""));
-        */
-
-        EscapeCharacters inserter = new EscapeCharacters();
-        System.out.println(inserter.quoteInserter());
-        System.out.println();
-        System.out.println("Print Arrays:");
-        System.out.print(inserter.quoteInserterMod());
-    }
+  public static void main(String[] args) {
+    EscapeCharacters inserter = new EscapeCharacters();
+    inserter.printQuotes();
+  }
 }
 
 
