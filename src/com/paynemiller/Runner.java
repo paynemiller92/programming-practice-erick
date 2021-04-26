f0b8c2141a2d438bf98d37198bc2e6c28b8e3ea0
Payne Miller <paynemiller7792@gmail.com>
2021-04-25 21:01:51 -0400

    Implement retirement calculator

--- src/com/paynemiller/Runner.java
+++ src/com/paynemiller/Runner.java
@@ -1,10 +1,24 @@
 package com.paynemiller;
 
-import com.paynemiller.ch2.Calculator;
+import com.paynemiller.ch2.EligibleToRetireException;
+import com.paynemiller.ch2.RetirementCalculator;
+
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        Calculator exercise = new Calculator();
-        exercise.doMath();
+        RetirementCalculator retirementCalculator = new RetirementCalculator();
+        try {
+            int retirementYear = retirementCalculator.calculateRetirementYear(promptForAge());
+            System.out.println("You can retire in " + retirementYear);
+        } catch (EligibleToRetireException e) {
+            System.out.println("You can already retire!");
+        }
+    }
+
+    private static int promptForAge() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your current age and press ENTER!");
+        return keyboard.nextInt();
     }
 }
