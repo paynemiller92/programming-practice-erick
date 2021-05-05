10afd82648f3eec894d5799af90e6168fccbdc23
Payne Miller <paynemiller7792@gmail.com>
2021-05-05 17:41:55 -0400

    Add switch statement for exercise chooser

--- src/com/paynemiller/Runner.java
+++ src/com/paynemiller/Runner.java
@@ -1,15 +1,71 @@
 package com.paynemiller;
 
-import com.paynemiller.ch3.PaintCalculator;
+import com.paynemiller.ch2.Calculator;
+import com.paynemiller.ch2.CharacterCounter;
+import com.paynemiller.ch2.EmptyWordException;
 
-import java.awt.*;
+import java.util.Scanner;
 
 public class Runner {
-    public static void main(String[] args) {
-       // PaintCalculator rectangle = new PaintCalculator();
-       // rectangle.getPaintNeeded();
 
-        PaintCalculator circle = new PaintCalculator();
-        circle.getPaintNeededForCircle();
+  private static Scanner keyboard = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    printMenu();
+    gatherInput();
+  }
+
+  private static void printMenu() {
+    System.out.println("Choose an exercise to demo: ");
+    System.out.println("----------------------------");
+    for (Exercise exercise : Exercise.values()) {
+      System.out.println(exercise.option + ". " + exercise.title);
     }
+
+    int selectedOption = gatherInput();
+    switch (selectedOption) {
+      case 1:
+        startCalculator();
+        break;
+      case 2: startCharacterCounter();
+      break;
+    }
+  }
+
+  private static int gatherInput() {
+    return keyboard.nextInt();
+  }
+
+  private static void startCalculator() {
+    Calculator calculator = new Calculator();
+    calculator.doMath();
+  }
+
+  private static void startCharacterCounter() {
+    System.out.println("Enter a word!");
+    String word = keyboard.nextLine();
+    CharacterCounter counter = new CharacterCounter();
+    try {
+      counter.countCharacters(word);
+    } catch (EmptyWordException e) {
+      e.printStackTrace(); // TODO: Gracefully handle exception.
+    }
+  }
+
+  private enum Exercise {
+
+    CALCULATOR("Calculator", 1),
+    CHARACTER_COUNTER("Character Counter", 2),
+    ESCAPE_CHARACTER_PRINTER("Escape Printer", 3),
+    QUOTER("Quoter", 4),
+    RETIREMENT_CALCULATOR("Retirement Calculator", 5);
+
+    private String title;
+    private int option;
+
+    Exercise(String title, int option) {
+      this.title = title;
+      this.option = option;
+    }
+  }
 }
